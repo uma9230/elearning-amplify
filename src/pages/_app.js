d723ad3e5954314c3606372066865015a7023c84
uma9230 <uma120613@gmail.com>
2023-10-31 12:12:58 +0530

    first commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import { Amplify, Auth } from "aws-amplify";
+import awsconfig from "../aws-exports";
+Amplify.configure(awsconfig);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
